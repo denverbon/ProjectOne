f397ab706d1734f28347698ddb1016adb08f5ae0
Denver Bon <denverbon@Denvers-iMac-Pro.local>
2021-01-26 11:01:22 -0800

    second steps

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let img;
+function preload() {
+  img = loadImage('stars.jpg');
+}
 function setup() {
-createCanvas(800,800);
+  createCanvas(675,1200);
+  image(img, 0, 0);
+
 }
 
 function draw() {
- ellipse(mouseX,mouseY,200,200);
+	fill(50);
+	stroke(0);
+	quad(300,300,150,450,300,600,450,450);
+
+    fill(250);
+	stroke(0);
+	quad(300,300,150,450,300,600,250,450);
 }
