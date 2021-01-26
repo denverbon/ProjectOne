1f90b8a3d0e681fd45895018323d4a544c1939dc
Denver Bon <denverbon@Denvers-iMac-Pro.local>
2021-01-26 11:54:10 -0800

    progress

--- sketch.js
+++ sketch.js
@@ -4,16 +4,30 @@ function preload() {
 }
 function setup() {
   createCanvas(675,1200);
-  image(img, 0, 0);
+ 
 
 }
 
 function draw() {
-	fill(50);
-	stroke(0);
-	quad(300,300,150,450,300,600,450,450);
+ image(img, 0, 0);
+	drawMoon(2);
 
+
+}
+
+function drawMoon(phase){
+  	let p4 = phase;
+    
+    fill(45);
+  	stroke(0);
+  	quad(300,300,150,450,300,600,450,450);
+
+    if (phase < 15){
+      p4 = map(phase,0,14,150,450);
+    
     fill(250);
-	stroke(0);
-	quad(300,300,150,450,300,600,250,450);
+  	stroke(0);
+  	quad(300,300,150,450,300,600,p4,450);
+  }
+    if (phase)
 }
